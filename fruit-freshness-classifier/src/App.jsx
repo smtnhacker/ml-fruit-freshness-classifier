3a9a564dacbcd2bf0e8f0f360f17386aaaf7c45b
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-06 15:04:41 +0800

    feat: add fastapi server

--- fruit-freshness-classifier/src/App.jsx
+++ fruit-freshness-classifier/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+
+axios
+  .get("http://127.0.0.1:8000")
+  .then((response) => {
+    console.log("Test");
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>FRUIT CLASSIFIER</h2>
+      <button>UPLOAD IMAGE</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
