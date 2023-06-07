90b3574f3122cff856b863d80ddc2eef81972838
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-06-08 00:41:16 +0800

    feat: add progress bar upon fetching result

--- fruit-freshness-classifier/src/App.jsx
+++ fruit-freshness-classifier/src/App.jsx
@@ -4,10 +4,15 @@ import { useRef, useState } from "react";
 import { AiFillFileImage } from 'react-icons/ai';
 
 
-const ImageForm = ({ setresult }) => {
+const ImageForm = () => {
+  const [result, setresult] = useState("");
   const [image, setImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false)
+
 
   const handleSubmit = async (event) => {
+    setIsLoading(true)
+
     setresult("");
     event.preventDefault();
     const imageInput = fileInput.current.files[0];
@@ -15,7 +20,7 @@ const ImageForm = ({ setresult }) => {
 
     const output = document.querySelector("output")
     let imageOutput = "";
-    
+
     console.log(imageInput)
     imageOutput = `<div class="image">
                 <img src="${URL.createObjectURL(imageInput)}" alt="image">
@@ -46,7 +51,7 @@ const ImageForm = ({ setresult }) => {
     } catch (error) {
       console.error("Failed to upload image", error);
     }
-
+    setIsLoading(false)
   };
 
 
@@ -56,27 +61,30 @@ const ImageForm = ({ setresult }) => {
 
 
   return (
-    <div onClick={()=> fileInput.current.click()} className="form">
-      <output><AiFillFileImage size={130} fill="#848b9a" /></output>
-      <input onChange={handleSubmit} ref={fileInput} type="file" name="image" accept="image/*" required style={{display:"none"}}/>
-      <p>Upload image here</p>
-
-    </div>
+    <>
+      <div onClick={()=> fileInput.current.click()} className="form">
+        <output><AiFillFileImage size={130} fill="#848b9a" /></output>
+        <input onChange={handleSubmit} ref={fileInput} type="file" name="image" accept="image/*" required style={{display:"none"}}/>
+        <p>Upload image here</p>
+      </div>
+      <div className="result">
+        {isLoading ? 
+          (<div className="progress-bar"><div className="progress-fill"></div></div>)
+          : 
+          (<span>{result === "" ? "" : result > 0.5 ? "This fruit is most likely: rotten" : "This fruit is most likely: fresh"}</span>)}
+      </div>
+    </>
 
   );
 };
 
 function App() {
-  const [result, setresult] = useState("");
 
   return (
     <div className="container">
       <h1>FRUIT CLASSIFIER</h1>
-      <ImageForm setresult={setresult} />
-      <p>
-        This fruit is most likely:{" "}
-        {result === "" ? "" : result > 0.5 ? "rotten" : "fresh"}
-      </p>
+      <p>Upload a photo of a fruit here to determine if it is rotten or not</p>
+      <ImageForm />  
     </div>
   );
 }
