fac12879697c6afe3510c86b4342221db075ff02
smtnhacker <themikosurara@gmail.com>
2023-06-12 23:11:16 +0800

    feat: add file drag and drop

--- fruit-freshness-classifier/src/components/ImageForm.jsx
+++ fruit-freshness-classifier/src/components/ImageForm.jsx
@@ -3,33 +3,20 @@ import axios from "axios";
 import { useRef, useState } from "react";
 
 const ImageForm = ({ API }) => {
+  const [imgSrc, setImgSrc] = useState("image.svg")
   const [result, setresult] = useState("");
   const [isLoading, setIsLoading] = useState(false)
   const fileInput = useRef(null)
 
-  const handleSubmit = async (event) => {
+  const submit = async (imageInput) => {
     setIsLoading(true)
-
     setresult("");
-    event.preventDefault();
-    const imageInput = fileInput.current.files[0];
-
-    const output = document.querySelector("output")
-    let imageOutput = "";
 
     console.log(imageInput)
-    imageOutput = `<div class="image">
-                <img src="${URL.createObjectURL(imageInput)}" alt="image">
-              </div>`
-
-    output.innerHTML = imageOutput;
+    setImgSrc(URL.createObjectURL(imageInput))
 
     const formData = new FormData();
     formData.append("image", imageInput);
-    for (const [key, value] of formData.entries()) {
-      console.log(key, value);
-    }
-
 
     try {
       const response = await axios.post(
@@ -49,14 +36,56 @@ const ImageForm = ({ API }) => {
       console.error("Failed to upload image", error);
     }
     setIsLoading(false)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const imageInput = fileInput.current.files[0];
+    await submit(imageInput)
+    
   };
 
+  const handleDrop = (e) => {
+    console.log("File(s) dropped", e)
+    e.preventDefault()
+    e.stopPropagation()
 
+    if (e.dataTransfer.items) {
+      [...e.dataTransfer.items].forEach(item => {
+        if (item.kind === "file") {
+          const file = item.getAsFile()
+          if (file.type.includes("image/")) {
+            submit(file)
+          }
+        }
+      })
+    }
+  }
+
+  const handleDragOver = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  const removeDrag = {
+    onDrag: e => e.preventDefault(),
+    onDrop: e => e.preventDefault(),
+    onDragEnd: e => e.preventDefault(),
+    onDragEnter: e => e.preventDefault(),
+    onDragLeave: e => e.preventDefault(),
+    onDragOver: e => e.preventDefault(),
+    onDragStart: e => e.preventDefault(),
+  }
 
   return (
     <>
-      <div onClick={()=> fileInput.current.click()} className="form">
-        <output><img src="/image.svg" /></output>
+      <div 
+        className={`form`}
+        onClick={()=> fileInput.current.click()}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+      >
+        <output><div {...removeDrag} className="image"><img {...removeDrag} src={imgSrc} /></div></output>
         <input onChange={handleSubmit} ref={fileInput} type="file" name="image" accept="image/*" required style={{display:"none"}}/>
         <p>Upload image here</p>
       </div>
